b6a10cb92bdcc2d38d17f0e26aaca7ec6729c379
UnusualProgramist <skutecznezycie@gmail.com>
2021-03-03 15:03:06 +0100

    Dodanie main.c

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "world.h"
+
+int main(int argc, char **argv) {
+	/* Opcje
+	** argv[2] == Moor lub M
+	** to jest 8 wokół z przekątnymi
+	** argv[2] == Neumann lub N
+	** to jest 4 wokół BEZ przekątnych
+	*/
+
+
+	return 0;
+}
