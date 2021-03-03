8eaf71b24f131c762964bcee42c6fd787eba1cdd
Majki1337 <mrefe12345@gmail.com>
2021-03-03 15:00:27 +0100

    Added world matrix map alongside with its library.

---
+++ world.h
@@ -0,0 +1,21 @@
+#include<stdlib.h>
+
+typedef struct {
+	int rn;
+	int cn;
+	double *world;
+} matrix_t;
+
+void free_matrix( matrix_t * );
+
+matrix_t * make_matrix( int rn, int cn );
+
+matrix_t * read_matrix( FILE *in );
+
+void write_matrix( matrix_t *, FILE *out );
+
+void put_entry_matrix( matrix_t *, int i, int j, double val );
+
+void add_to_entry_matrix( matrix_t *, int i, int j, double val );
+
+double get_entry_matrix( matrix_t *, int i, int j );
