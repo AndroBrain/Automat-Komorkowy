db92599fbd6be1bdcfe5dac7a35dcb2b7d44b1cb
UnusualProgramist <skutecznezycie@gmail.com>
2021-03-04 11:00:26 +0100

    A little bit cleaner looking code

--- main.c
+++ main.c
@@ -7,29 +7,10 @@
 // 1 - alive
 // 2 - dying
 // 3 - breed
-void breedAndKill( matrix_t *mat, int row, int column, int alive) {
-	int actualState = get_entry_matrix( mat, row, column );
-	if( actualState == 0 )
-		if( alive == 3 )
-			put_entry_matrix( mat, row, column, 3);
-	if( actualState == 1 )
-		if( alive != 2 || alive != 3 )
-			put_entry_matrix( mat, row, column, 2);
 
-}
-
-void neumann(matrix_t *mat, int row, int column){
-	int alive = 0;
-	for( int r = row - 1; r <= row + 1; r++ )
-		for( int c = column - 1; c <= column + 1; c++)
-			if( r >= 0 && r < mat->rn && c >= 0 && c < mat->cn && ( r != row || c != column)) {
-				int state = get_entry_matrix( mat, r, c);	
-				if( state == 1 || state == 2) {
-					alive++;
-				}
-			}
-	breedAndKill( mat, row, column, alive);
-}
+void breedAndKill( matrix_t *mat, int row, int column, int alive );
+void neumann( matrix_t *mat, int row, int column );
+void createPbmFile( matrix_t *mat, char *name );
 
 int main(int argc, char **argv) {
 	/* Opcje
@@ -49,4 +30,31 @@ int main(int argc, char **argv) {
 
 	write_matrix( mat, stdout );
 	return 0;
+}
+
+void neumann(matrix_t *mat, int row, int column){
+	int alive = 0;
+	for( int r = row - 1; r <= row + 1; r++ )
+		for( int c = column - 1; c <= column + 1; c++)
+			if( r >= 0 && r < mat->rn && c >= 0 && c < mat->cn && ( r != row || c != column)) {
+				int state = get_entry_matrix( mat, r, c);	
+				if( state == 1 || state == 2) {
+					alive++;
+				}
+			}
+	breedAndKill( mat, row, column, alive);
+}
+
+void breedAndKill( matrix_t *mat, int row, int column, int alive) {
+    int actualState = get_entry_matrix( mat, row, column );
+    if( actualState == 0 )
+        if( alive == 3 )
+            put_entry_matrix( mat, row, column, 3);
+    if( actualState == 1 )
+        if( alive != 2 || alive != 3 )
+            put_entry_matrix( mat, row, column, 2);
+}
+
+void createPbmFile( matrix_t *mat, char *name ){
+
 }
