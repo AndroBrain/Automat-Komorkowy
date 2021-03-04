f8a604d5b9c319407e6367344d0eda64a72f03ad
UnusualProgramist <skutecznezycie@gmail.com>
2021-03-04 11:39:49 +0100

    Added saving in pbm and is function isAlive

--- main.c
+++ main.c
@@ -9,8 +9,10 @@
 // 3 - breed
 
 void breedAndKill( matrix_t *mat, int row, int column, int alive );
+void moor( matrix_t *mat, int row, int column );
 void neumann( matrix_t *mat, int row, int column );
-void createPbmFile( matrix_t *mat, char *name );
+void createPbmFile( matrix_t *mat, char *fileName );
+int isAlive( int state );
 
 int main(int argc, char **argv) {
 	/* Opcje
@@ -26,35 +28,57 @@ int main(int argc, char **argv) {
 
 	write_matrix( mat, stdout );
 
-	neumann( mat, 1, 1 );
+	neumann( mat, 1, 3 );
+
+//	createPbmFile( mat, fileName );
 
 	write_matrix( mat, stdout );
 	return 0;
 }
 
-void neumann(matrix_t *mat, int row, int column){
+void moor(matrix_t *mat, int row, int column){
 	int alive = 0;
-	for( int r = row - 1; r <= row + 1; r++ )
-		for( int c = column - 1; c <= column + 1; c++)
-			if( r >= 0 && r < mat->rn && c >= 0 && c < mat->cn && ( r != row || c != column)) {
-				int state = get_entry_matrix( mat, r, c);	
-				if( state == 1 || state == 2) {
-					alive++;
-				}
-			}
+	for ( int r = row - 1; r <= row + 1; r++ )
+		for ( int c = column - 1; c <= column + 1; c++)
+			if ( r >= 0 && r < mat->rn && c >= 0 && c < mat->cn && ( r != row || c != column)) 
+				if ( isAlive( get_entry_matrix( mat, r, c ) ) == 1 )
+					alive ++;
 	breedAndKill( mat, row, column, alive);
 }
 
-void breedAndKill( matrix_t *mat, int row, int column, int alive) {
-    int actualState = get_entry_matrix( mat, row, column );
-    if( actualState == 0 )
-        if( alive == 3 )
-            put_entry_matrix( mat, row, column, 3);
-    if( actualState == 1 )
-        if( alive != 2 || alive != 3 )
-            put_entry_matrix( mat, row, column, 2);
+void neumann( matrix_t *mat, int row, int column ){
+	int alive = 0;
+	if ( row - 1 >= 0 && column - 1 >=0 ) {
+		int state = get_entry_matrix( mat, row - 1, column - 1 );
+
+}
 }
 
-void createPbmFile( matrix_t *mat, char *name ){
+int isAlive( int state ){
+	if( state == 1 || state == 2) {
+		return 1;
+	}
+	return 0;
+}
 
+void breedAndKill( matrix_t *mat, int row, int column, int alive) {
+	int actualState = get_entry_matrix( mat, row, column );
+	if( actualState == 0 )
+		if( alive == 3 )
+			put_entry_matrix( mat, row, column, 3);
+	if( actualState == 1 )
+		if( alive != 2 || alive != 3 )
+			put_entry_matrix( mat, row, column, 2);
+}
+
+void createPbmFile( matrix_t *mat, char *fileName ){
+	FILE *out = fopen( fileName, "wb" );
+	fprintf( out, "P1\n" );
+	fprintf( out, "%d %d\n", mat->rn, mat->cn );
+
+	for(int r = 0; r < mat->rn; r++){
+		for( int c = 0; c < mat->cn; c++)
+			fprintf( out, "%d ", get_entry_matrix( mat, r, c ));
+		fprintf( out, "\n" );
+	}
 }
