95aa5ac1bff9cf228a9d76dacb4d2dbfee7905d9
Majki1337 <mrefe12345@gmail.com>
2021-03-11 16:20:50 +0100

    Fixed pbm file formating

--- savingFiles/output.c
+++ savingFiles/output.c
@@ -1,13 +1,16 @@
 #include "../world/mat.h"
 
-void write_matrix (matrix_t * m, FILE * out) {
+void write_matrix (matrix_t * m, FILE * out, int mode) {
 	int i, j;
 	if (m == NULL) {
 		fprintf (out, "Matrix is NULL\n");
 		return;
 	}
+	if ( mode == 1 )
+		fprintf (out, "%d %d\n", m->rn, m->cn);
+	if ( mode == 2 )
+		fprintf (out, "%d %d\n", m->cn, m->rn);
 
-	fprintf (out, "%d %d\n", m->rn, m->cn);
 	for (i = 0; i < m->rn; i++) {
 		for (j = 0; j < m->cn - 1; j++)
 			fprintf (out, "%d ", m->e[i * m->cn + j]);
@@ -17,7 +20,7 @@ void write_matrix (matrix_t * m, FILE * out) {
 
 void saveLastIteration( matrix_t *mat ) {
 	FILE *out = fopen( "last.txt", "w" );
-	write_matrix( mat, out );
+	write_matrix( mat, out, 1 );
 }
 
 void createPbmFile( matrix_t *mat, int number ){
@@ -28,7 +31,7 @@ void createPbmFile( matrix_t *mat, int number ){
 	FILE *out = fopen( fileName, "wb" );
 	fprintf( out, "P1\n" );
 
-	write_matrix( mat, out );	
+	write_matrix( mat, out, 2 );	
 
 	saveLastIteration( mat );
 }
