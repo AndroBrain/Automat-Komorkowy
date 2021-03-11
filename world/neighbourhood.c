c8ba5ada567bbe864c7241021f7e4633686803fb
Majki1337 <mrefe12345@gmail.com>
2021-03-11 13:25:07 +0100

    Fixing previous commit

--- world/neighbourhood.c
+++ world/neighbourhood.c
@@ -30,3 +30,14 @@ void moor(matrix_t *mat, int row, int column){
 					alive ++;
 	breedAndKill( mat, row, column, alive);
 }
+
+void neumann(matrix_t *mat, int row, int column){
+	int alive = 0;
+	for ( int r = row - 1; r <= row + 1; r++ )
+		for ( int c = column -1; c <= column + 1; c++)
+			if ( r >= 0 && r < mat->rn && c >= 0 && c < mat->cn && ( r != row || c != column))
+				if ( (c == column && r == row - 1 ) || (c == column + 1 && r == row ) || (c == column && r == row + 1) || (c == column - 1 && r == row) )
+					if ( isAlive( get_entry_matrix( mat, r, c ) ) == 1 )
+						alive++;
+	breedAndKill( mat, row, column, alive);
+}
