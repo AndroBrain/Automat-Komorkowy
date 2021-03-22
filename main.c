65fa6035a61f14791cf50c35c3f143e6faea708b
UnusualProgramist <skutecznezycie@gmail.com>
2021-03-22 12:48:57 +0100

    Added instruction when user gives the wrong input

--- main.c
+++ main.c
@@ -7,20 +7,25 @@
 // 1 - alive
 // 2 - dying
 // 3 - breed
+void showInstructionOnError() {
+	fprintf( stderr, "Instruction\nGive the following arguments:\n1.File name\n2.Number of Iterations\n");
+}
 
 int main(int argc, char **argv) {
 	if ( argc < 3 ) {
-		fprintf( stderr, "Not enough arguments, please give:\n1.File name\n2.Number of Iterations\n" );
+		fprintf( stderr, "Not enough arguments, please give:\n" );
+		showInstructionOnError();
 		return 1;
 	}	
-
 	int numberOfIterations = atoi(argv[2]);
-	char *fileName = argv[1];
-
-	if ( numberOfIterations < 1 ) { 
+	
+	if ( numberOfIterations <= 0 ) { 
 		fprintf( stderr, "The number of iterations must be bigger than 0\n" );
+		showInstructionOnError();
 		return 2;
 	}
+
+	char *fileName = argv[1];
 
 	makeAutomata( fileName, numberOfIterations );
 
